67a5f8d3666bb96e69082f76e886f2bd7ad29e0a
test <test>
2017-03-20 15:03:06 +0530

    Arrow fucntions

---
+++ es-5-app/src/index_basic_arrow.js
@@ -0,0 +1,12 @@
+//ES style of function declarations
+
+//ES 5
+var greet=function(){
+    console.log("Greet");
+}
+greet();
+
+//ES 6 Style --> Arrow functions
+
+var greetArrow=()=>console.log('Greet Arrow');
+greetArrow();
