e55d6053a2b098602dfa2fe7cd7989bf20b78cbf
test <test>
2017-03-23 17:38:03 +0530

    Event Branch

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,89 +1,51 @@
-//Different Layouts
-import { Component } from '@angular/core';
-//import { TRAINERS } from './trainer_mock';
-import { Trainer } from './app.trainer';
-import { TrainerService } from './trainer.service';
 
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>
-  <ul class="trainers">
-    <li [class.selected]="trainer===selectedTrainer" (click)="onSelect(trainer)" *ngFor="let trainer of trainers">
-      <span class="badge">{{trainer.id}}</span>
-      {{trainer.name}}
-    </li>
-  </ul>
-  
-  <trainer-detail [trainerInfo]="selectedTrainer"></trainer-detail>
-  `,
-  styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .trainers {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-    float:left;
-  }
-  .trainers li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .trainers li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .trainers li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .trainers .text {
-    position: relative;
-    top: -3px;
-  }
-  .trainers .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }`]
+  template: `<button (click)="clicked($event)">Click ME</button><br>
+                <input (keyup)="onKey($event)">
+                <p>{{values}}</p><br>
+               <!-- <input #val (keyup)="test"> //test can be any name-->
+               <input #val (keyup)="test(val.value)">     
+           
+                <p>{{val.value}}</p>
+                <input #box 
+                (keyup.enter)="onEnter(box.value)" (blur)="onEnter(box.value)">
+                <p>{{value}}</p>
+                Which Key: {{which}}`,
+    host:{'(window:keydown)':'hotkeys($event)'}
+
 })
 export class AppComponent {
-  title: string = "Trainer Management System"
-  trainers: Trainer[];
-  constructor(private trainerService: TrainerService) {
-    this.trainerService.getTrainerDetails().then(trainers => this.trainers = trainers);
-    console.log(this.trainers);
-    // let promise=this.trainerService.getTrainerDetails(); //simplified version
-    // promise.then(cb);
+  clicked($event: Event) {
+    console.log($event);
   }
-  //trainers = this.trainerService.getTrainerDetails();
+  values: string = '';
+  // onKey(event: any) {
+  //   this.values = event.target.value;
+  //   // console.log(event);
+  // }
+  onKey(event: KeyboardEvent) {
+    //this.values = event.target.value;    //this is javscript  style
+    this.values = (<HTMLInputElement>event.target).value; //this is typescript style -->defining type of UI type
+  }
+  myval:string=''; 
+  test(value:string){
+    this.myval=value;
+  }
 
-  selectedTrainer: Trainer; //if initially this is not given a value then the template will throw an error
-  onSelect(trainer: Trainer): void {
-    this.selectedTrainer = trainer;
-    console.log('clicked', trainer);
-  }
+  value = 'Old Value';
+    onEnter(value: string) {
+        console.log('Event is called!');
+        this.value = value;
+    }
+    which:string="";
+    hotkeys(event:any){
+      this.which=event.which; //which is pre-defined key word
+      console.log(event.which);
+    }
 
 }
 
